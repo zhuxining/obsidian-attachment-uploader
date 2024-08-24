b01582ee0196955046c10c8c8d859a74abcfeb4d
xy <xiyao@xxxx.com>
2024-08-24 18:23:40 +0800

    fix attachment not image

--- src/main.ts
+++ src/main.ts
@@ -110,7 +110,8 @@ export default class AttachmentUploader extends Plugin {
 
 	private updateEditorContent(editor: MarkdownFileInfo, attachment: Attachment, newUrl: string) {
 		const content = editor.editor?.getValue() ?? "";
-		const updatedContent = content.replace(attachment.source, `![${attachment.alt}](${newUrl})`);
+		// TODO: check if image, if not remove ! keep to []()
+		const updatedContent = content.replace(attachment.source, `[${attachment.basename}](${newUrl})`);
 		editor.editor?.setValue(updatedContent);
 	}
 
